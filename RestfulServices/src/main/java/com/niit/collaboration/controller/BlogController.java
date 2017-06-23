1722d4dbbab0c932fad6795d8b8637e2427dbec3
Indhuvel <Indhuvel@users.noreply.github.com>
2017-06-23 10:34:50 +0530

    Services

---
+++ RestfulServices/src/main/java/com/niit/collaboration/controller/BlogController.java
@@ -0,0 +1,71 @@
+package com.niit.collaboration.controller;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.niit.collaboration.dao.BlogDAO;
+import com.niit.collaboration.model.Blog;
+
+@RestController
+public class BlogController {
+	
+	@Autowired
+	BlogDAO blogDAO;
+	
+  	
+   @RequestMapping(value="/Blog", method=RequestMethod.GET)
+   public ResponseEntity<List<Blog>> Blog() {
+	List<Blog> listblog = blogDAO.list();
+	  
+	return new ResponseEntity<List<Blog>>(listblog, HttpStatus.OK);
+    }
+	
+	@RequestMapping(value ="/insertBlog", method = RequestMethod.POST)
+	public ResponseEntity<String> insertBlog(@RequestBody Blog blog) {
+		blog.setCreatedate(new Date());
+		blog.setStatus("NA");
+		blog.setLikes(0);
+
+		blogDAO.insert(blog);
+		return new ResponseEntity<String>("Successfully Inserted", HttpStatus.OK);
+	}
+
+	@RequestMapping(value ="/deleteBlog/{blogid}", method = RequestMethod.DELETE)
+	public ResponseEntity<String> deleteBlog(@PathVariable("blogid") int blogid) {
+		blogDAO.delete(blogid);
+		return new ResponseEntity<String>("Deleted Blog Successfully", HttpStatus.OK);
+	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@RequestMapping(value="/updateBlog/{blogid}",method=RequestMethod.PUT)
+	public ResponseEntity<Blog> updateBlog(@PathVariable("blogid") int blogid,@RequestBody Blog blog)
+	{
+		blog = blogDAO.saveOrUpdate(blog);
+		if (null == blog) {
+			return new ResponseEntity("No Blog found for ID " + blogid, HttpStatus.NOT_FOUND);
+		}
+
+		return new ResponseEntity(blog, HttpStatus.OK);
+	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@RequestMapping(value ="/getAllBlog/{blogid}", method = RequestMethod.GET)
+	public ResponseEntity<List<Blog>> getAllBlog(@PathVariable("blogid") int blogid) {
+		
+		Blog blog = blogDAO.getAllBlog(blogid);
+		if (blog == null) {
+			return new ResponseEntity("No Blog found for ID " + blogid, HttpStatus.NOT_FOUND);
+		}
+
+		return new ResponseEntity(blog, HttpStatus.OK);
+     }
+}
