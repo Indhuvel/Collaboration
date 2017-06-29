f815744cb8efced9fc1564810b2f5a5f3d84decb
Indhuvel <Indhuvel@users.noreply.github.com>
2017-06-29 18:21:23 +0530

    FrontEnd

---
+++ CollaborationFront/WebContent/app.js
@@ -0,0 +1,24 @@
+var app = angular.module("myApp", [ 'ngRoute' ]);
+
+app.config(function($routeProvider) {
+	$routeProvider
+	.when('/', {
+		templateUrl : 'Home/Home.html'/*index.html'*/	
+			})
+
+	.when('/login', {
+		templateUrl : 'User/Login.html'
+
+	})
+   .when('/register', {
+		templateUrl : 'User/Register.html'
+
+	})
+	.when('/blog', {
+		templateUrl : 'Blog/Blog.html'
+
+	})
+	.otherwise({
+		resirectTo : '/'
+	});
+});
